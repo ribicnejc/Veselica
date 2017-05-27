5cc19c11278134749db1ce3cf7fa727e15fbcd99
Nejc Ribic <nejc.ribic95@gmail.com>
2017-05-28 00:28:03 +0200

    Videos implemented

---
+++ app/src/main/java/com/ribic/nejc/veselica/VideosAdapter.java
@@ -0,0 +1,71 @@
+package com.ribic.nejc.veselica;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.ribic.nejc.party.R;
+import com.ribic.nejc.veselica.objects.Video;
+
+import java.util.ArrayList;
+
+public class VideosAdapter extends RecyclerView.Adapter<VideosAdapter.VideoViewHolder> {
+
+    private ArrayList<Video> mVideos;
+    private final VideosAdapter.TrailersAdapterOnClickHandler mClickHandler;
+
+    public interface TrailersAdapterOnClickHandler {
+        void trailersOnClick(int clickedItemIndex);
+    }
+
+
+    public VideosAdapter(ArrayList<Video> mTrailers, TrailersAdapterOnClickHandler mClickHandler) {
+        this.mClickHandler = mClickHandler;
+        this.mVideos = mTrailers;
+    }
+
+    @Override
+    public VideoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.video_item, parent, false);
+        return new VideoViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(VideoViewHolder holder, int position) {
+        String imgUrl = mVideos.get(position).getVideoUrl();
+        Glide.with(holder.itemView.getContext()).load(imgUrl).into(holder.mVideoImage);
+        holder.mVideoTitle.setText(mVideos.get(position).getTitle());
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mVideos.size();
+    }
+
+    class VideoViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView mVideoTitle;
+        ImageView mVideoImage;
+
+        private VideoViewHolder(View itemView) {
+            super(itemView);
+            mVideoTitle = (TextView) itemView.findViewById(R.id.text_view_video_title_item);
+            mVideoImage = (ImageView) itemView.findViewById(R.id.image_view_item_thumbnail);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int clickedPosition = getAdapterPosition();
+            mClickHandler.trailersOnClick(clickedPosition);
+        }
+    }
+}
--- app/src/main/java/com/ribic/nejc/veselica/ui/DetailActivity.java
+++ app/src/main/java/com/ribic/nejc/veselica/ui/DetailActivity.java
@@ -2,6 +2,9 @@ package com.ribic.nejc.veselica.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -11,32 +14,45 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.ribic.nejc.party.R;
+import com.ribic.nejc.veselica.VideosAdapter;
 import com.ribic.nejc.veselica.fragments.MainEventsFragment;
+import com.ribic.nejc.veselica.objects.Video;
 import com.ribic.nejc.veselica.utils.NetworkUtils;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class DetailActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class DetailActivity extends AppCompatActivity implements VideosAdapter.TrailersAdapterOnClickHandler{
 
     public final String TAG = "nejc";//DetailActivity.this.getSimpleName();
-    public TextView mTextViewTitle;
     public TextView mTextViewDate;
     public TextView mTextViewLocation;
     public TextView mTextViewRegion;
     public TextView mTextViewActors;
     public TextView mTextViewAbout;
+    public ArrayList<Video> mVideos;
+    public RecyclerView mRecyclerView;
+    public VideosAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         String href = getIntent().getStringExtra(MainEventsFragment.EXTRA_HREF);
-        mTextViewTitle = (TextView) findViewById(R.id.text_view_title);
         mTextViewDate = (TextView) findViewById(R.id.text_view_date);
         mTextViewActors = (TextView) findViewById(R.id.text_view_actors);
         mTextViewLocation = (TextView) findViewById(R.id.text_view_location);
         mTextViewAbout = (TextView) findViewById(R.id.text_view_about);
         mTextViewRegion = (TextView) findViewById(R.id.text_view_region);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view_item);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        mRecyclerView.setLayoutManager(layoutManager);
+
         fetchData(href);
     }
 
@@ -49,6 +65,7 @@ public class DetailActivity extends AppCompatActivity {
 
                     @Override
                     public void onResponse(JSONObject response) {
+                        mVideos = new ArrayList<>();
                         try{
                             String title = response.getString("title");
                             String date = response.getString("date");
@@ -56,16 +73,25 @@ public class DetailActivity extends AppCompatActivity {
                             String location = response.getString("location");
                             String region = response.getString("region");
                             String about = response.getString("about");
-
-                            mTextViewTitle.setText(title);
+                            setTitle(title);
                             mTextViewDate.setText(date);
                             mTextViewActors.setText(actors);
                             mTextViewLocation.setText(location);
                             mTextViewRegion.setText(region);
                             mTextViewAbout.setText(about);
+                            JSONArray jsonArray = response.getJSONArray("videos");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                String thumb = jsonObject.getString("thumbnail_url");
+                                String videoTitle = jsonObject.getString("title");
+                                Video video = new Video(videoTitle, thumb);
+                                mVideos.add(video);
+                            }
                         }catch (Exception e){
-
+                            Log.i(TAG, e.getLocalizedMessage());
                         }
+                        mAdapter = new VideosAdapter(mVideos, DetailActivity.this);
+                        mRecyclerView.setAdapter(mAdapter);
                     }
                 }, new Response.ErrorListener() {
 
@@ -75,5 +101,10 @@ public class DetailActivity extends AppCompatActivity {
             }
         });
         mRequestQueue.add(jsonObjReq);
+    }
+
+    @Override
+    public void trailersOnClick(int clickedItemIndex) {
+
     }
 }
